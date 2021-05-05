6681e334a8ccd27bc6b52d431ac6b432b9e467a7
dejanzivanov <77687815+dejanzivanov@users.noreply.github.com>
2021-05-05 22:45:03 +0000

    Deploying to gh-pages from master @ 85ac3e964b1d8bd0cd0465a6b69daf36980832e2 🚀

--- Floor1/auditorium.js
+++ Floor1/auditorium.js
@@ -3,26 +3,11 @@ let wobotAuditoriumPopup = 'wobot_auditorium_popup';
 let wobotAuditoriumText = "Welcome hitchiker to our Auditorium. Todays topic is: 'What is the answer to the Ultimate Question of Life, the Universe, and Everything?'.";
 
 
-function ajax(a,b,c){//url,function,just a placeholder
-c=new XMLHttpRequest;
-c.open('GET',a);
-c.onload=b;
-c.send()
-}
-
-function alertTxt(){
-console.log(this.response)
-}
-
-window.onload=function(){
-ajax('placeholder.txt',alertTxt)
-}
-
 WA.onEnterZone(wobotAuditoriumZone, () => {
     currentPopup =  WA.openPopup(wobotAuditoriumPopup, wobotAuditoriumText,[])
 })
 
-WA.onLeaveZone(arrowZoneName, closePopUp);
+WA.onLeaveZone(wobotAuditoriumZone, closePopUp);
 
 function closePopUp(){
     if (currentPopup !== undefined) {
