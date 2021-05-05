fa27d5cda50737dd1bcee05c4b9cc79623298fd5
Dejan Zivanov <punisher0504@gmail.com>
2021-05-06 00:55:06 +0200

    Adding silent zones, typo fix in auditorium.js

--- Floor1/auditorium.js
+++ Floor1/auditorium.js
@@ -1,6 +1,6 @@
 let wobotAuditoriumZone = 'wobot_auditorium_zone';
 let wobotAuditoriumPopup = 'wobot_auditorium_popup';
-let wobotAuditoriumText = "Welcome hitchiker to our Auditorium. Todays topic is: 'What is the answer to the Ultimate Question of Life, the Universe, and Everything?'.";
+let wobotAuditoriumText = "Welcome hitchiker to our Auditorium. Today\'s topic is: 'What is the answer to the Ultimate Question of Life, the Universe, and Everything?'.";
 
 
 WA.onEnterZone(wobotAuditoriumZone, () => {
